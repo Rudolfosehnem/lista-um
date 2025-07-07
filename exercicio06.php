e5c7e4a81895a060a163644421372b7d1e2a89c2
Rudolfosehnem <rudolfo_silva@estudante.sesisenai.org.br>
2025-07-07 12:02:34 -0300

    update-exercicio06

--- exercicio06.php
+++ exercicio06.php
@@ -1,40 +1,26 @@
-<!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fatorial</title>
+    <title>Exibir divisores de um número</title>
 </head>
-
 <body>
-    <h1>Fatorial</h1>
-
     <form method="POST" action="">
-        <label for="fatorial">Digite um número para descobrir seu FATORIAL:</label>
-        <input type="number" id="fatorial" name="fatorial" required>
-        <button type="submit" name="verificar_fatorial">Verificar</button>
+        <label for="verificar_divisores">Digite um número para verificar seu divisor:</label>
+        <input type="number" name="numero_divisor" id="numero_divisor" required>
+        <button type="submit" name="verificar_divisor">Verificar</button>
     </form>
-
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['verificar_fatorial'])) {
-        $numero = $_POST['fatorial'];
-    };
-    function loopFatorial($numero) {
-        $fatorial = 1;
-
-        for($i = 1; $i <= $numero; $i++) {
-            $fatorial *= $i;
+    <?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(isset($_POST['verificar_divisor'])){
+            $numero = $_POST['numero_divisor'];
+            for($i = 1; $i < $numero; $i++){
+                if($numero % $i === 0){
+                    echo "O número $i é divisor de $numero<br>\n";
+                }
+            }
         }
-
-        return $fatorial;
-
-    };
-    
-         echo "O fatorial do $numero é: " . loopFatorial($numero) . "\n";
-};
-?>
-
+    }
+    ?>
 </body>
 </html>
